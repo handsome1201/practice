f0ddba712aaa6829e3fc73917fcd415f877ad823
handsome1201 <handsome1201@naver.com>
2021-02-14 16:48:05 +0900

    피땀눈물34일차

---
+++ start/practice33/Coordinate.java
@@ -0,0 +1,28 @@
+package practice33;
+
+class Coordinate {
+	private double x;
+	private double y;
+
+	Coordinate(double x, double y){
+		this.x=x;
+		this.y=y;
+	}
+
+	double getX() {
+		return x;
+	}
+	double getY() {
+		return y;
+	}
+	void setX() {
+		this.x=x;
+	}
+	void setY() {
+		this.y=y;
+	}
+	void set(double x , double y) {
+		this.x=x;
+		this.y=y;
+	}
+}
